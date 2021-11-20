e45467de6f2ad4d79993e6754794d34bcd8ef490
Shubham Nivasarkar <74969994+shubhamnivasarkar@users.noreply.github.com>
2021-11-20 07:59:11 +0530

    Create PickNSaveMartApplication.java

---
+++ PickNSave-Mart/src/main/java/com/app/PickNSaveMartApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickNSaveMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickNSaveMartApplication.class, args);
+	}
+
+}
